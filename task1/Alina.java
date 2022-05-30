e52fa260031cdf670e31ea17aedef04ff34611df
Alina Dubrovina <dubrovinaa389@gmail.com>
2022-05-30 06:32:43 +0300

    first commit

---
+++ task1/Alina.java
@@ -0,0 +1,7 @@
+public class Alina {
+    public static void main(String ...args) {
+        System.out.println("Java: version 11.");
+        System.out.println("Java");
+        System.out.println("Java");
+    }
+}
